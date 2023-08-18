0ee08958d5bd66ca1560d6a3448edc33d338a7bc
seulGit <seulgi9421@gmail.com>
2023-08-18 23:06:31 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술은:
    
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    *롬복

---
+++ src/main/java/com/fc/projectboard/StudyFcProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fc.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyFcProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyFcProjectBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fc/projectboard/StudyFcProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyFcProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
