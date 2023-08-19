b0c8cc0fe36dbf4b6019f8fe4fc9274cbad23f5c
seulGit <109563083+seulGit@users.noreply.github.com>
2023-08-19 21:52:02 +0900

    #10 - 객체파일 주석 수정
    필드명에 대한 주석이 누락돼 수정하였음

--- src/main/java/com/fc/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fc/projectboard/domain/ArticleComment.java
@@ -5,11 +5,11 @@ import java.time.LocalDateTime;
 public class ArticleComment {
     private Long id;
     private Article article; // 게시글 (ID)
-    private String content;
+    private String content; // 본문
 
     // meta-data
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    private LocalDateTime createdAt; // 생성일시
+    private String createdBy; // 생성자
+    private LocalDateTime modifiedAt; // 수정일시
+    private String modifiedBy; // 수정자
 }
