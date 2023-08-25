797d71cf63eb6acbaeb0e06f2275d476ae923fe8
seulGit <seulgi9421@gmail.com>
2023-08-25 10:09:32 +0900

    #14 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로 엔티티 클래스에 들어가는 요소이고
    도메인과 직접 관련이 없는 요소이므로
    추출이 가능하다.
    
    '@MappedSuperClass 를 이용해서 변경점을 확인하고 이용하여 상속 방식으로 추출함
    그외 DateTimeFormat

--- src/test/java/com/fc/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/fc/projectboard/repository/JpaRepositoryTest.java
@@ -5,14 +5,12 @@ import com.fc.projectboard.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
