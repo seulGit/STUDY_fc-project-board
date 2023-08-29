1c576cf53aea4453ccb68555232191ec846b1fff
seulGit <seulgi9421@gmail.com>
2023-08-29 17:29:44 +0900

    #18 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한 번에 적용

--- src/main/java/com/fc/projectboard/controller/ArticleController.java
+++ src/main/java/com/fc/projectboard/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.fc.projectboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,5 +16,12 @@ public class ArticleController {
     public String articles(ModelMap map) {
         map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        map.addAttribute("article", "article"); // TODO: 구현할 때 여기에 실제 데이터 넣어줘야 한다.
+        map.addAttribute("articleComments", List.of());
+        return "articles/detail";
     }
 }
