227ab5cacab1ec524a4f74166c5d4d021a2a41c2
seulGit <seulgi9421@gmail.com>
2023-08-29 14:04:24 +0900

    #18 - 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다.

---
+++ src/main/java/com/fc/projectboard/controller/ArticleController.java
@@ -0,0 +1,10 @@
+package com.fc.projectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/article")
+@Controller
+public class ArticleController {
+
+}
