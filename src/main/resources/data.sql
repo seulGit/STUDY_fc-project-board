00f4d08f1a364e966262ebde02cc10000a5183df
seulGit <seulgi9421@gmail.com>
2023-08-30 22:12:15 +0900

    userAccount 추가
    강의에서 userAccount 관련 내용 추가하는 걸 생략하셔서 내가 그냥 따라 써 넣음

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,10 @@
+-- 테스트 계정
+-- TOD0: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
+insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
+('skoh', 'asdf1234', 'skoh',
+'skoh@gmail.com', 'I am skoh.', now(), 'skoh', now (), 'skoh');
+
+
 -- 123 게시글
 insert into article (title, content, created_by, modified_by, created_at, modified_at) values
 ('Quisque ut erat.', 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
