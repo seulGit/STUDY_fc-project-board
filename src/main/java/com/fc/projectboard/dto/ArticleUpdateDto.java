1533b10295623d1734e5365f6403852a97e428bc
seulGit <seulgi9421@gmail.com>
2023-09-02 17:00:34 +0900

    #20 - 게시글 서비스 로직의 테스트와 골격 잡기
    다음의 기능을 테스트로 표현
    
    * 게시글 검색하면 게시글 리스트 반환
    * 게시글을 ID로 조회
    * 게시글 생성
    * 게시글 수정
    * 게시글 삭제
    
    필요한 dto 와 enum을 함께 작성했는데,
    추후에 수정할 수도 있음

---
+++ src/main/java/com/fc/projectboard/dto/ArticleUpdateDto.java
@@ -0,0 +1,7 @@
+package com.fc.projectboard.dto;
+
+public record ArticleUpdateDto(String title, String content, String hashtag) {
+    public static ArticleUpdateDto of(String title, String content, String hashtag) {
+        return new ArticleUpdateDto(title, content, hashtag);
+    }
+}
