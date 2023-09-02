52351de51e445ca181a689a21ac0683bd3399b0a
seulGit <seulgi9421@gmail.com>
2023-09-02 18:39:28 +0900

    #20 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    강의에서 보여주지 않은 나머지 로직의 테스트 코드를 작성하고
    연관된 DTO 를 재설계함
    테스트가 필요로 하는 repository 쿼리 메소드도 작성

--- src/main/java/com/fc/projectboard/service/ArticleService.java
+++ src/main/java/com/fc/projectboard/service/ArticleService.java
@@ -2,15 +2,13 @@ package com.fc.projectboard.service;
 
 import com.fc.projectboard.domain.type.SearchType;
 import com.fc.projectboard.dto.ArticleDto;
-import com.fc.projectboard.dto.ArticleUpdateDto;
+import com.fc.projectboard.dto.ArticleWithCommentsDto;
 import com.fc.projectboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional
@@ -19,19 +17,19 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
         return Page.empty();
     }
 
     @Transactional(readOnly = true)
-    public ArticleDto searchArticle(long l) {
+    public ArticleWithCommentsDto getArticle(Long articleId) {
         return null;
     }
 
     public void saveArticle(ArticleDto dto) {
     }
 
-    public void updateArticle(long ArticleId, ArticleUpdateDto dto) {
+    public void updateArticle(ArticleDto dto) {
     }
 
     public void deleteArticle(long articleId) {
